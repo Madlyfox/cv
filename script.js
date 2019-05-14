be3428979f386e47c99bb7b64d95f6496a161f56
stanley <borromee.stanley@gmail.com>
2019-05-14 16:39:09 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var MyApp= angular.module('MyApp',['ngRoute']);
+
+	MyApp.config(function($routeProvider){
+
+		$routeProvider
+			.when('/main', {
+				templateUrl: 'views/Main.html'
+		
+			})
+			.when('/planetes', {
+				templateUrl: 'views/Planetes.html'
+		
+			})
+			.when('/etoiles', {
+				templateUrl: 'views/Étoiles.html'
+		
+			})
+			.when('/lunes', {
+				templateUrl: 'views/Lunes.html'
+		
+			})
+			.otherwise({
+				retirectTo: '/main'
+			})
+	})
